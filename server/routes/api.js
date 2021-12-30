43f3537070dc64d4ca3361bddb3136d1e3d3c39a
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2021-12-29 23:19:14 -0500

    updated api.js to add commented next steps

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,9 +11,11 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup', userController.signup, (req, res) => {
   // should redirect client to homepage after successful signup
+  // steps: signup middleare to add user data to database, cookie middleware to set a cookie, then render home app
 });
 // need to add set cookie middleware after signup middleware
 
 router.post('/login', userController.login, (req, res) => {
   // need to add set cookie middleware after login middleware
+  // steps: login middleware to verify user data in db, cookie middleware to set cookie, render home app
 });
