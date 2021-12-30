973e26b85cf8c84a873592c1cdd54d092966772c
Crystal Pederson <crystalpederson88@gmail.com>
2021-12-30 09:05:52 -0800

    added browser router for dashboard and login

--- client/App.js
+++ client/App.js
@@ -1,5 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
   const [state, updateState] = useState({
@@ -8,10 +12,14 @@ function App() {
   });
 
   return (
-    <div>
-      <h1>Go-Pool App Component</h1>
-      <p>hello world</p>
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        <Dashboard />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+    </Switch>
   );
 }
 
