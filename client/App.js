36779565fae19142b45d7b4ea91a622ff0179eb0
Crystal Pederson <crystalpederson88@gmail.com>
2021-12-30 09:57:36 -0800

    added more routes to react router

--- client/App.js
+++ client/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Messages from './components/Messages';
 
 function App() {
   const [state, updateState] = useState({
@@ -18,6 +19,12 @@ function App() {
       </Route>
       <Route path='/login'>
         <Login />
+      </Route>
+      {/* <Route path='/signup'>
+        <Signup />
+      </Route> */}
+      <Route path='/messages'>
+        <Messages />
       </Route>
     </Switch>
   );
