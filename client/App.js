9bda71bc4bf5a68515825a1051a8c33a810d2810
Crystal Pederson <crystalpederson88@gmail.com>
2021-12-29 18:05:29 -0800

    Set up initial hmtl files
    Co-authored by: Crystal Pederson <88516047+crystalpederson@users.noreply.github.com>
    Co-authored by: Jane Park <88516047+crystalpederson@users.noreply.github.com>

--- client/App.js
+++ client/App.js
@@ -1,2 +1,29 @@
-// import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 // import './style.css';
+
+// class App extends Component{
+//   render(){
+//       return(
+//           <div>
+//               <h1>Go-Pool App Component</h1>
+//           </div>
+//       )
+//   }
+// }
+
+function App() {
+    const [state, updateState] = useState(
+        {
+            userId: '',
+            status: false,
+        }
+    )
+
+    return (
+        <div>
+              <h1>Go-Pool App Component</h1>
+          </div>
+    )
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
