77e64257fc32b9ef9c9d3ea8ad0a4e48ba8d1b62
James Kim <james.minjae.97@gmail.com>
2021-12-29 21:25:40 -0500

    minor changes to apps.jsx and index.jsx

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-import styles from './style.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
