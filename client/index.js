c8a0a0da716b631280bf93d5191b1bea103fea5d
James Kim <james.minjae.97@gmail.com>
2021-12-29 20:29:46 -0500

    first commit

---
+++ client/index.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV;
+
+module.exports = {
+  mode,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development Go Pool',
+      template: './client/index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      publicPath: '/',
+      directory: path.join(__dirname, 'build'),
+    },
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
