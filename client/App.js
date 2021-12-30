a521edc8a4ad04b37467c9e07221236fa3ca32d5
James Kim <james.minjae.97@gmail.com>
2021-12-29 21:39:03 -0500

    webpack is working

--- client/App.js
+++ client/App.js
@@ -1,2 +1,18 @@
-// import React, { useState, useEffect } from "react";
-// import './style.css';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [state, updateState] = useState({
+    userId: '',
+    status: false,
+  });
+
+  return (
+    <div>
+      <h1>Go-Pool App Component</h1>
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,3 +1,9 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './App';
+
+import styles from './style.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
