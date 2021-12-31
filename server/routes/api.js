9e2cb6d2c6aca9b3cf45a96f032cbdb69bbcc3ab
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2021-12-30 22:46:40 -0500

    completed signup

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,20 +2,21 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController');
 const messageController = require('../controllers/messageController');
+const sessionController = require('../controllers/sessionController');
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  // render the signup page in client folder
-});
-
 router.post('/signup', userController.signup, (req, res) => {
-  // should redirect client to homepage after successful signup
-  // steps: signup middleare to add user data to database, cookie middleware to set a cookie, then render home app
+  // steps: signup middleare to add user data to database, session middleware, cookie middleware to set a cookie
+  // inputs: first_name, last_name, email, password, address, phoneNumber
+  res.status(200).send('Account successfully created')
 });
-// need to add set cookie middleware after signup middleware
 
 router.post('/login', userController.login, (req, res) => {
   // need to add set cookie middleware after login middleware
-  // steps: login middleware to verify user data in db, cookie middleware to set cookie, render home app
+  // steps: login middleware to verify user data in db, session middleware, cookie middleware to set cookie
+  // inputs: email, password
+  res.status(200).send('Successfully logged in')
 });
+
+module.exports = router; 
