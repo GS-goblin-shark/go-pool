48fcad09c68faccf39b881f2e07db52ff2dc45d8
Crystal Pederson <crystalpederson88@gmail.com>
2021-12-31 11:05:05 -0800

    tried adding a calendar to the dashboard

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar'
+import moment from 'moment';
 
 function Dashboard() {
+  const [dateState, setDateState] = useState(new Date())
+  const changeDate = (e) => {
+    setDateState(e)
+  }
+
   return (
-    <div>
-      <h1>This is the dashboard component</h1>
+    <div id='dashboardComponent'>
+      <h1>Upcoming Events</h1>
+      <div id='calendar-div'>
+        <Calendar value={dateState} onChange={changeDate}/>
+        <p>Events for <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+      </div>
     </div>
   );
 }
