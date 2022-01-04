90e957befd45a01d357f30e6d33043b9b11c226d
James Kim <james.minjae.97@gmail.com>
2022-01-03 21:57:55 -0500

    removed multer

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const multer = require('multer');
 const userController = require('../controllers/userController');
 const cookieController = require('../controllers/cookieController');
 const messageController = require('../controllers/messageController');
 const sessionController = require('../controllers/sessionController');
 
 const router = express.Router();
-const parser = multer().none();
 
-router.post('/signup', parser, userController.signup, (req, res) => {
+router.post('/signup', userController.signup, (req, res) => {
   // steps: signup middleare to add user data to database, session middleware, cookie middleware to set a cookie
   // inputs: first_name, last_name, email, password, address, phoneNumber
   res.status(200).send('Account successfully created');
 });
 
-router.post('/login', parser, userController.login, (req, res) => {
+router.post('/login', userController.login, (req, res) => {
   // need to add set cookie middleware after login middleware
   // steps: login middleware to verify user data in db, session middleware, cookie middleware to set cookie
   // inputs: email, password
