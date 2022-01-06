fb2e17fff694841207cbbf7909dcd899f20ff6b9
James Kim <james.minjae.97@gmail.com>
2022-01-05 22:44:24 -0500

    started message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,8 +2,38 @@ const db = require('../models/userModel');
 
 const messageController = {};
 
-messageController.getMessages = (req, res, next) => {
-  return next();
+messageController.getMessages = async (req, res, next) => {
+  const { from_email, to_email } = req.query;
+  let from_userid, to_userid;
+
+  const queryUser =
+    'SELECT users._id, users.email FROM users WHERE email = $1 OR email = $2';
+  const paramUser = [from_email, to_email];
+
+  const queryMessages =
+    'SELECT * FROM messages WHERE from_user_id = $1 or to_user_id = $2';
+  const paramMessages = [];
+
+  try {
+    const userID = await db.query(queryUser, paramUser);
+    userID.rows[0].email === from_email
+      ? (from_userid = userID.rows[0].email)
+      : (to_userid = userID.rows[0].email);
+    userID.rows[1].email === from_email
+      ? (from_userid = userID.rows[1].email)
+      : (to_userid = userID.rows[1].email);
+    paramMessages.push(from_userid);
+    paramMessages.push(to_userid);
+    console.log(paramMessages);
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error with messageController.getMessages Error: ${err}`,
+      message: {
+        err: 'Error with the backend middlware function',
+      },
+    });
+  }
 };
 
 messageController.storeMessage = (req, res, next) => {
--- server/routes/message.js
+++ server/routes/message.js
@@ -3,12 +3,14 @@ const express = require('express');
 const messageController = require('../controllers/messageController');
 
 const router = express.Router();
+const { getMessages, storeMessage } = messageController;
 
-router.get('/', (req, res) => {
+router.get('/', getMessages, (req, res) => {
+  //   return res.status(200).json(res.locals.messages);
   return res.sendStatus(200);
 });
 
-router.post('/', (req, res) => {
+router.post('/', storeMessage, (req, res) => {
   return res.sendStatus(200);
 });
 
