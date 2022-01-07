f32478f2224fd4cdfe3ec65fe48820482520f480
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-01-07 00:31:49 -0500

    untested query to post res to thread

--- server/controllers/threadReplyController.js
+++ server/controllers/threadReplyController.js
@@ -1,7 +1,32 @@
+const { db } = require("../models/sessionModel");
+
 const threadReplyController = {};
 
 threadReplyController.postThreadReply = async (req, res, next) => {
+    const { email, thread, date_posted, thread_id, response } = req.body;
 
+    const queryEmail = 'SELECT u._id FROM users u WHERE email = $1;';
+    const emailParams = [email];
+
+    const queryRes = 'INSERT INTO threads (thread, date, user_id, event_id, thread_id) VALUES ($1, $2, $3, $4, $5) RETURNING threads.thread, threads.date;';
+    const resParams = [thread, date_posted];
+
+    try {
+        const userID = await db.query(queryEmail, emailParams);
+        resParams.push(userID.rows[0]._id);
+
+        const threadRes = await db.query(queryRes, resParams);
+        res.locals.threadRes = threadRes.rows[0];
+
+        return next();
+    } catch (err) {
+        return next({
+            log: `Error in threadReplyController.postThreadReply: ${err}`,
+            message: {
+                err: `Error in the backend from threadReplyController.postThreadReply`
+            }
+        });
+    }
 };
 
 module.exports = threadController;
