4a2b032267034a8b96673b96659be056d5779a39
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-01-08 01:45:20 -0500

    delete specific response to a query by thread id

--- server/routes/threadReply.js
+++ server/routes/threadReply.js
@@ -2,11 +2,17 @@ const express = require('express');
 const threadReplyController = require('../controllers/threadReplyController');
 
 const router = express.Router();
-const { postThreadReply } = threadReplyController;
+const { postThreadReply, deleteThreadReply } = threadReplyController;
 
 router.post('/', postThreadReply, (req, res) => {
     return res
     .sendStatus(200);
 });
+
+router.delete('/:id', deleteThreadReply, (req, res) => {
+    return res
+    .status(200)
+    .send('Thread message successfully deleted')
+})
 
 module.exports = router;
